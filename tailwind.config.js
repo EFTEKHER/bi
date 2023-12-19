246a3f1d52b9b1c380fdc9059121976524943d65
EFTEKHER <eftekherali2000@gmail.com>
2023-12-19 21:47:51 +0600

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./assets", "./pages/**/*.{html,js}","./pages/product.html ","./style","./src/**/*.{css,js,html}"],
+  content: [
+    "./index.html",
+    "./assets",
+    "./pages/**/*.{html,js}",
+    "./pages/product.html ",
+    "./style",
+    "./src/**/*.{css,js,html}",
+    "./pages/achievement.html",
+  ],
   theme: {
-    extend: {
-    
-     
-    },
+    extend: {},
   },
   plugins: [],
 };
